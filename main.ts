70fbc6431d2d1990fb83529b468e3edf74dbf246
Bobinen <91364071+Bobinen@users.noreply.github.com>
2021-09-25 20:58:17 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let move_or_stop = 0
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    if (pins.analogReadPin(AnalogPin.P0) >= 100 && move_or_stop == 0) {
+        move_or_stop = 1
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    }
+    if (pins.analogReadPin(AnalogPin.P0) >= 100 && move_or_stop == 0) {
+        move_or_stop = 0
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
